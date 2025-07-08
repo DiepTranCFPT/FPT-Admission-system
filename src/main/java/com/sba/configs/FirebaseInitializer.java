85e8f155d321d4dcd2bd607b05a7a5dab7e81bc6
Diep Tran <trancaodiepnct@gmail.com>
2025-07-08 15:17:06 +0700

    fix code

--- src/main/java/com/sba/configs/FirebaseInitializer.java
+++ src/main/java/com/sba/configs/FirebaseInitializer.java
@@ -5,8 +5,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,20 +14,18 @@ import java.io.InputStream;
 @Service
 public class FirebaseInitializer {
 
-    private final String firebaseConfigPath;
-
-    public FirebaseInitializer(@Value("${firebase.config.path}") String firebaseConfigPath) {
-        this.firebaseConfigPath = firebaseConfigPath;
-    }
+    @Value("${firebase.config.path}")
+    private Resource firebaseConfigPath;
 
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = new ClassPathResource(firebaseConfigPath).getInputStream();
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(com.google.auth.oauth2.GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-            FirebaseApp.initializeApp(options);
+            try (InputStream serviceAccount = firebaseConfigPath.getInputStream()) {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(com.google.auth.oauth2.GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                FirebaseApp.initializeApp(options);
+            }
         }
         return FirebaseAuth.getInstance();
     }
