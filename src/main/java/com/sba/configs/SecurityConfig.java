7c8f418f04d2443a808196ad8fcc70570a0243d3
Diep Tran <trancaodiepnct@gmail.com>
2025-07-11 00:11:51 +0700

    fix

--- src/main/java/com/sba/configs/SecurityConfig.java
+++ src/main/java/com/sba/configs/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
                         .requestMatchers(PUBLIC_ENDPOINTS_METHOD).hasAnyRole( "ADMIN")
                         .requestMatchers("/api/upload-image").permitAll()
                         .requestMatchers("/uploads/**").permitAll()
+                        .requestMatchers("/api/tickets").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
